b7b4147d378e78068d7d6e1ca4d59bc5d65bf4e7
Miguel Sánchez Martin <miguelsanchezmartin54@gmail.com>
2025-12-15 19:37:04 +0100

    añadimos la parte de web

---
+++ web/models/GenreModel.mjs
@@ -0,0 +1,8 @@
+export class Genre {
+  constructor({ id, name, created_at, updated_at } = {}) {
+    this.id = id;
+    this.name = name;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
+}
